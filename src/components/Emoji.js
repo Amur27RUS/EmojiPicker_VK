5fa054308e108bbfb124970c1106655eb6273da8
Amur27RUS <Klyuev_Andrey@mail.ru>
2021-06-19 21:36:01 +0300

    added 'all' and 'recent' emoji functionality

---
+++ src/components/Emoji.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './emojiPicker.scss';
+
+const Emoji = ({recentEmoji, setRecentEmoji, symbol, type, setInputValue, inputValue}) => (
+    <span
+        className="emoji"
+        role="img"
+        onClick={()=> {
+            if(type === 'all'){
+                let isNotInRecent = true;
+
+                //Проверяем, чтобы нашего emoji не было в недавних:
+                recentEmoji.map((emoji, index) =>{
+                    if(emoji === symbol){
+                        isNotInRecent = false;
+                        console.log(recentEmoji);
+                        console.log(recentEmoji.splice(index, 1));
+                        //Если эмодзи уже есть в использованных, то добавляем его просто в начало недавних
+                        setRecentEmoji([symbol, ...recentEmoji.slice(0, index), ...recentEmoji.slice(index, 25)]);
+                    }
+                });
+
+                if(isNotInRecent) {
+                    //Если уже есть 25 элементов, то добавляем новый в начало и убираем один из конца
+                    if (recentEmoji.length === 25) {
+                        setRecentEmoji([symbol, ...recentEmoji.slice(0, 24)])
+                    }else {
+                        setRecentEmoji([symbol, ...recentEmoji]);
+                    }
+                }
+
+
+            }
+
+            //Добавляем эмодзи в инпут
+            setInputValue(inputValue + symbol);
+        }}
+    >
+        {symbol}
+    </span>
+);
+export default Emoji;
